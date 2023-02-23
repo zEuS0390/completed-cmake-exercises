5038a2873b5ce9e6bc7e26ac6a5122d5761256c1
zEuS0390 <zeusjamesbaltazar@gmail.com>
2023-02-23 11:31:03 +0800

    inital commit

---
+++ Step3/build/CMakeFiles/Tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tutorial.dir/tutorial.cxx.obj"
+  "CMakeFiles/Tutorial.dir/tutorial.cxx.obj.d"
+  "Tutorial.exe"
+  "Tutorial.exe.manifest"
+  "Tutorial.pdb"
+  "libTutorial.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
