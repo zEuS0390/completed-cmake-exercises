3dc0f0d6aa4089ab92ed4923ee1099b7a1a0c992
zEuS0390 <zeusjamesbaltazar@gmail.com>
2023-03-08 10:35:19 +0800

    update step 8: adding a custom command and generated file

--- Step8/MathFunctions/mysqrt.cxx
+++ Step8/MathFunctions/mysqrt.cxx
@@ -1,7 +1,8 @@
-#include <cmath>
+// #include <cmath>
 #include <iostream>
 
 #include "MathFunctions.h"
+#include "Table.h"
 
 // a hack square root calculation using simple operations
 double mysqrt(double x)
@@ -10,23 +11,22 @@ double mysqrt(double x)
     return 0;
   }
 
-  // if we have both log and exp then use them
-#if defined(HAVE_LOG) && defined(HAVE_EXP)
-  double result = std::exp(std::log(x) * 0.5);
-  std::cout << "Computing sqrt of " << x << " to be " << result
-            << " using log and exp" << std::endl;
-#else
+  // use the table to help find an initial value
   double result = x;
-
-  // do ten iterations
-  for (int i = 0; i < 10; ++i) {
-    if (result <= 0) {
-      result = 0.1;
-    }
-    double delta = x - (result * result);
-    result = result + 0.5 * delta / result;
-    std::cout << "Computing sqrt of " << x << " to be " << result << std::endl;
+  if (x >= 1 && x < 10) {
+	  std::cout << "Use the table to help find an initial value " << std::endl;
+	  result = sqrtTable[static_cast<int>(x)];
+  } else {
+	  // do ten iterations
+	  for (int i = 0; i < 10; ++i) {
+	    if (result <= 0) {
+	      result = 0.1;
+	    }
+	    double delta = x - (result * result);
+	    result = result + 0.5 * delta / result;
+	    std::cout << "Computing sqrt of " << x << " to be " << result << std::endl;
+	  }
   }
-#endif
+
   return result;
 }
